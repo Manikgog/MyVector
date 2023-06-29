04ba645f7030d81af0d3b7d8b014c1132418db16
Manikgog <m4xin4t4@gmail.com>
2023-06-29 15:05:12 +0500

    Add project files.

---
+++ Vector.h
@@ -0,0 +1,235 @@
+﻿/*!
+* \brief Шаблонный класс динамической коллекции на основе динамического массива.
+
+Создать шаблонный класс динамической коллекции (связный список, очередь динамический
+массив).
+Обязятельными пунктами задания являются следующие возможности коллекции:
+- размещение в себе элементов произвольного типа;
+- добавление и удаление элементов;
+- предоставление доступа к элементам массива;
+- возможность работы с колеекцией через range based for, std::size;
+- корректная передача по параметру в функции.
+Дополнительные возможности:
+работа независимо с хранением данных и с выделением памяти;
+возможность удаления и вставки новых элементов в произвольные места коллекции.
+*/
+#ifndef _VECTOR_H_
+#define _VECTOR_H_
+
+template <typename T>
+class Vector
+{
+public:
+	///////////////////////////////////////////////////////////////////////////////////////
+	class Iterator
+	{
+	public:
+		Iterator() = delete;
+		Iterator(T* data) : data_(data) {}
+		Iterator& operator++()
+		{
+			data_++;
+			return *this;
+		}
+
+		Iterator& operator++(int)
+		{
+			Iterator tmp(*this);
+			data_++;
+			return tmp;
+		}
+
+		Iterator& operator--()
+		{
+			data_--;
+			return *this;
+		}
+
+		Iterator& operator--(int)
+		{
+			Iterator tmp(*this);
+			data_++;
+			return tmp;
+		}
+
+		Iterator& operator+(int num)
+		{
+			Iterator it(this->data_ + num);
+			(*this) = it;
+			return *this;
+		}
+
+		Iterator& operator-(int num)
+		{
+			Iterator it(this->data_ - num);
+			(*this) = it;
+			return *this;
+		}
+
+		bool operator==(const Iterator& obj) const
+		{
+			return this->data_ == obj.data_;
+		}
+
+		bool operator!=(const Iterator& obj) const
+		{
+			return this->data_ != obj.data_;
+		}
+
+		T& operator*()
+		{
+			return *data_;
+		}
+
+
+	private:
+		T* data_;
+	};
+	///////////////////////////////////////////////////////////////////////////////////
+
+	Vector() : _array(nullptr), _size(0) {}
+
+	Vector(const Vector& v) : _array(new T[v._size]), _size(v._size)
+	{
+		for (size_t i = 0; i < _size; i++)
+		{
+			_array[i] = v._array[i];
+		}
+	}
+
+	~Vector()
+	{
+		this->clear();
+	}
+
+	Vector& operator=(const Vector& v)
+	{
+		if (this != &v)
+		{
+			if (this->_array != nullptr)
+				this->_array = nullptr;
+			this->_size = v._size;
+			for (size_t i = 0; i < this->_size; i++)
+			{
+				this->_array[i] = v._array[i];
+			}
+			return *this;
+		}
+		return *this;
+	}
+
+	void push_back(const T& data)
+	{
+		this->_size++;
+		T* newArray = new T[this->_size]{};
+		for (size_t i = 0; i < this->_size - 1; i++)
+		{
+			newArray[i] = this->_array[i];
+		}
+		newArray[this->_size - 1] = data;
+		delete[] this->_array;
+		this->_array = newArray;
+	}
+
+	void pop_back()
+	{
+		this->_size--;
+		T* newArray = new T[this->_size]{};
+		for (size_t i = 0; i < this->_size; i++)
+		{
+			newArray[i] = this->_array[i];
+		}
+		delete[] this->_array;
+		this->_array = newArray;
+	}
+
+	void insert(const T& data, int position)
+	{
+		this->_size++;
+		T* newArray = new T[this->_size]{};
+		for (size_t i = 0; i < position; ++i)
+		{
+			newArray[i] = this->_array[i];
+		}
+		newArray[position] = data;
+		for (size_t i = position; i < this->_size - 1; ++i)
+		{
+			newArray[i + 1] = this->_array[i];
+		}
+		delete[] this->_array;
+		this->_array = newArray;
+	}
+
+	void clear()
+	{
+		delete[] _array;
+		_array = nullptr;
+	}
+
+	T front()
+	{
+		return *_array;
+	}
+
+	int size() const
+	{
+		return _size;
+	}
+
+	T& operator[](int i)
+	{
+		return _array[i];
+	}
+
+	const T& operator[](int i) const
+	{
+		return _array[i];
+	}
+
+	T& at(int i) 
+	{
+		return _array[i];
+	}
+
+	const T& at(int i) const
+	{
+		return _array[i];
+	}
+
+	Iterator begin()
+	{
+		return Iterator(_array);
+	}
+
+	Iterator begin() const
+	{
+		return Iterator(_array);
+	}
+
+	Iterator end()
+	{
+		return Iterator(_array + _size);
+	}
+
+	Iterator end() const
+	{
+		return Iterator(_array + _size);
+	}
+
+	Iterator rbegin()
+	{
+		return Iterator(_array + _size - 1);
+	}
+
+	Iterator rend()
+	{
+		return Iterator(_array - 1);
+	}
+
+
+private:
+	T* _array;
+	int _size;
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,191 @@
+﻿#include <cassert>
+#include <iostream>
+#include <locale.h>
+#include <windows.h>
+#include <cstdlib>
+#include <vector>
+#include "Vector.h"
+
+void TestPushBack()
+{
+	Vector<int> vec;
+	vec.push_back(12);
+	assert(vec[0] == 12 && vec.size() == 1);
+	vec.push_back(10);
+	assert(vec[1] == 10 && vec.size() == 2);
+	vec.push_back(8);
+	assert(vec[2] == 8 && vec.size() == 3);
+	vec.push_back(6);
+	assert(vec[3] == 6 && vec.size() == 4);
+	vec.push_back(4);
+	assert(vec[4] == 4 && vec.size() == 5);
+	vec.push_back(2);
+	assert(vec[5] == 2 && vec.size() == 6);
+	vec.push_back(0);
+	assert(vec[6] == 0 && vec.size() == 7);
+}
+
+void TestPopBack()
+{
+	Vector<int> vec;
+	vec.push_back(12);
+	vec.push_back(10);
+	vec.push_back(8);
+	vec.push_back(6);
+	vec.push_back(4);
+	vec.push_back(2);
+	vec.push_back(0);
+	vec.pop_back();
+	assert(vec.size() == 6);
+	vec.pop_back();
+	assert(vec.size() == 5);
+	vec.pop_back();
+	assert(vec.size() == 4);
+	vec.pop_back();
+	assert(vec.size() == 3);
+	vec.pop_back();
+	assert(vec.size() == 2);
+	vec.pop_back();
+	assert(vec.size() == 1);
+	vec.pop_back();
+	assert(vec.size() == 0);
+}
+
+void TestFront()
+{
+	Vector<int> vec;
+	vec.push_back(12);
+	vec.push_back(10);
+	vec.push_back(8);
+	vec.push_back(6);
+	vec.push_back(4);
+	vec.push_back(2);
+	vec.push_back(0);
+	assert(vec.front() == 12);
+}
+
+void TestInsert()
+{
+	Vector<int> vec;
+	vec.push_back(12);
+	vec.push_back(10);
+	vec.push_back(8);
+	vec.push_back(6);
+	vec.push_back(4);
+	vec.push_back(2);
+	vec.push_back(0);
+	vec.insert(21, 0);
+	assert(vec.size() == 8);
+	assert(*(vec.begin() + 0) == 21);
+	vec.insert(7, 3);
+	assert(vec.size() == 9);
+	assert(*(vec.begin() + 3) == 7);
+	vec.insert(-1, 9);
+	assert(vec.size() == 10);
+	assert(*(vec.begin() + 9) == -1);
+}
+
+void TestAt()
+{
+	Vector<int> vec;
+	vec.push_back(12);
+	vec.push_back(10);
+	vec.push_back(8);
+	vec.push_back(6);
+	vec.push_back(4);
+	vec.push_back(2);
+	vec.push_back(0);
+	for (int i = 0; i < vec.size(); i++)
+	{
+		assert(vec[i] == vec.at(i));
+	}
+}
+
+void Test_rbegin()
+{
+	Vector<int> vec;
+	vec.push_back(12);
+	vec.push_back(10);
+	vec.push_back(8);
+	vec.push_back(6);
+	vec.push_back(4);
+	vec.push_back(2);
+	vec.push_back(0);
+	assert(*(vec.rbegin()) == 0);
+	assert(vec.rbegin() == vec.end() - 1);
+}
+
+void Test_end()
+{
+	Vector<int> vec;
+	vec.push_back(12);
+	vec.push_back(10);
+	vec.push_back(8);
+	vec.push_back(6);
+	vec.push_back(4);
+	vec.push_back(2);
+	vec.push_back(0);
+	assert(vec.begin() + vec.size() == vec.end());
+}
+
+void Test()
+{
+	TestPushBack();
+	TestPopBack();
+	TestFront();
+	TestInsert();
+	TestAt();
+	Test_rbegin();
+	Test_end();
+}
+
+
+
+
+template <typename T>
+void printVector(T& v)
+{
+	bool is_first = true;
+	for (const auto& elem : v)
+	{
+		if (!is_first)
+			std::cout << ", ";
+		is_first = false;
+		std::cout << elem;
+	}
+	std::cout << std::endl;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251); 
+	Test();
+	
+
+	Vector<int> vec;
+	
+	vec.push_back(12);
+	vec.push_back(10);
+	vec.push_back(8);
+	vec.push_back(6);
+	vec.push_back(4);
+	vec.push_back(2);
+	vec.push_back(0);
+
+	//printVector(vec);
+	std::cout << "vec.begin() + 2 = " << *(vec.begin() + 2) << std::endl;
+	std::cout << "vec.end() - 1 = " << *(vec.end() - 1) << std::endl;
+	//vec.pop_back();
+
+	vec.insert(7, 3);
+
+	for (auto it = vec.rbegin(); it != vec.rend(); --it)
+	{
+		std::cout << *it << ' ';
+	}
+	std::cout << std::endl;
+
+	std::cout << "vec.front() = " << vec.front() << std::endl;
+	
+	return 0;
+}
